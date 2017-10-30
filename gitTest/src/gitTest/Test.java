d74b49dd1638a629edaf07ffc8ec812893430712
student <student@M110111>
2017-10-30 17:11:53 +0900

    original(2017-10-30) project: gitTest

---
+++ gitTest/src/gitTest/Test.java
@@ -0,0 +1,14 @@
+package gitTest;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+		
+		/*
+		 * 프로젝트 작업으로 치면
+		 * 기능 명세등이 다나오고 나서
+		 * 필요한 클래스들을 (스켈레톤)
+		 * 생성만 한 상태...가 적당
+		 */
+	}
+}
